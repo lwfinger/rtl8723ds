b062cc88743cb738fab310f696431e50482c65c0
Larry Finger <Larry.Finger@lwfinger.net>
2023-08-01 12:37:02 -0500

    rtl8723ds: Fix warning in do_div
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -342,7 +342,8 @@ static u64 rtw_get_systime_us(void)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 20, 0))
 	ktime_t ts;
 	ts = ktime_get_boottime();
-	return do_div(ts, 1000);	
+	do_div(ts, (uint32_t)1000);
+	return ts;
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39))
 	struct timespec ts;
 	get_monotonic_boottime(&ts);
